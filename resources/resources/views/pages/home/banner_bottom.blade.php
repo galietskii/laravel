b4d2fae6acfa299c2ccd95d51279e12f08e2ccbd
galietskii <galietskiy2@gmail.com>
2022-10-25 01:29:11 +0300

    25.10.2002 1:26

--- resources/resources/views/pages/home/banner_bottom.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- banner-bottom -->
-<div class="banner-bottom">
-    <div class="container">
-        @foreach($banner_bottoms as $banner_bottom)
-            {!! $banner_bottom['text'] !!}
-        @endforeach
-    </div>
-</div>
-<!-- //banner-bottom -->
